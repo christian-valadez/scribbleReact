2c08aaf44194fabb96ad6bbb6b7401137be5e714
Christian Valadez <cvaladez@stanford.edu>
2018-03-10 12:44:38 -0800

    printing out letters now

--- App.js
+++ App.js
@@ -1,27 +1,99 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Dimensions, StyleSheet, Text, View } from 'react-native';
 
 import RNDraw from 'rn-draw'
   
 
+const {height, width} = Dimensions.get('window');
+
 
 
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = { xCoords: [], 
+      yCoords: []
+    }
+  }
 
   _printStrokes = (currentPoints) => { 
     console.log(currentPoints)
+
+    let xCoord = []
+    let yCoord = []
+
+    currentPoints.map((val, index) => {
+      xCoord.push(val.x)
+      yCoord.push(val.y)
+    })
+
+    console.log(xCoord)
+    console.log(yCoord)
+
+    let xCoords = this.state.xCoords.slice()
+    let yCoords = this.state.yCoords.slice()
+    xCoords.push(xCoord)
+    yCoords.push(yCoord)
+
+    this.setState({ xCoords: xCoords, yCoords: yCoords })
+  }
+
+  _getResults = () => {
+    let trace = [] 
+    this.state.xCoords.map((val, index) => {
+      let stroke = [] 
+      stroke.push(this.state.xCoords[index])
+      stroke.push(this.state.yCoords[index])
+      trace.push(stroke)
+    })
+    
+    console.log(trace)
+    let data = JSON.stringify({
+      "options": "enable_pre_space",
+      "requests": [{
+          "writing_guide": {
+              "writing_area_width": width || undefined,
+              "writing_area_height": height || undefined
+          },
+          "ink": trace,
+          "language": "en"
+      }]
+    })
+    fetch("https://www.google.com.tw/inputtools/request?ime=handwriting&app=mobilesearch&cs=1&oe=UTF-8", {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: data
+      })
+        .then((response) => {
+          console.log(response)
+          const text = response._bodyText 
+          const js = JSON.parse(text)
+          console.log(js[1][0][1])          
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+        
+    //Clear the state for the next letter
+    this.setState({xCoords: [], yCoords: []})
   }
 
   render() {
     return (
-      <RNDraw
-        containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
-        rewind={ (undo) => {this._undo = undo}}
-        clear={(clear) => {this._clear = clear} }
-        color={'#000000'}
-        strokeWidth={4}
-        strokes={this._printStrokes}
-      />
+
+        <RNDraw
+          containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+          rewind={ (undo) => {this._undo = undo}}
+          clear={(clear) => {this._clear = clear} }
+          color={'#000000'}
+          strokeWidth={4}
+          strokes={this._printStrokes}
+          getLetter={this._getResults}
+        />
+
     );
   }
 }
